44453a683197b557b1059e09211f1973814f3b9f
BorisBrogle <boris.brogle@gmail.com>
2018-05-12 12:08:53 +0200

    Added won detection function

--- Plateau.cpp
+++ Plateau.cpp
@@ -23,7 +23,58 @@ void Plateau::moveFromTo(int fromX, int fromY, int toX, int toY) {
   gameMatrix[fromX][fromY] = 0;
 }
 
-
 bool Plateau::isFreeAt(int x, int y) {
   return this->gameMatrix[x][y]==0;
+}
+
+int Plateau::hasSomeoneWon() {
+  int currentVal = 0;
+  bool firstTested = false;
+  bool secondTested = false;
+  for(int x=0; x<this->nbCells; x++) {
+    for(int y=0; y<this->nbCells; y++) {
+      currentVal = this->gameMatrix[x][y];
+      if(currentVal == 1 && !firstTested) { //we find the first pawn of the first player
+        firstTested = true;
+        return this->testPattern(x,y,currentVal);
+      } else if(currentVal == 2 && !secondTested) { //we find the first pawn of the second player
+        secondTested = true;
+        return this->testPattern(x,y,currentVal);
+      }
+    }
+  }
+  return 0;
+}
+
+int Plateau::testPattern(int x, int y, int currentVal) {
+  //Stores the different configurations to win
+  int pattern[5][3][2] = {
+    {{1,1},{2,2},{3,3}}, //diag1
+    {{-1,-1},{-2,-2},{-3,-3}}, //diag2
+    {{1,0},{0,1},{1,1}}, //square
+    {{1,0},{2,0},{3,0}}, //horizontal
+    {{0,1},{0,2},{0,3}} //vertical
+  };
+
+  for(int pat=0; pat<5; pat++) {
+    bool testBool = true;
+    int coord = 0;
+    do {
+      int testX = x+pattern[pat][coord][0];
+      int testY = y+pattern[pat][coord][1];
+      if(testX>=0 && testX<this->nbCells && testY>=0 && testY<this->nbCells) { //if we're outside of the gameMatrix
+        if(this->gameMatrix[testX][testY] != currentVal) {
+          testBool = false;
+        }
+      } else {
+        testBool = false;
+      }
+      coord++;
+    } while(testBool == true && coord <3);
+
+    if(testBool == true) { //if test bool is still at true, we have found a victory configuration
+      return currentVal;
+    }
+  }
+  return 0; //we have tested all the patterns, none of them were "victorious", so the currentVal player has not won yet
 }
--- Plateau.hpp
+++ Plateau.hpp
@@ -11,14 +11,24 @@ class Plateau {
     int nbCells; //number of cells in the game
     vector<vector<int>> gameMatrix; //it'll contain the locations of the different "Pions"
 
+    int testPattern(int x, int y, int currentVal);
+
   public:
     Plateau(){};
     Plateau(int nbCells);
     ~Plateau(){};
 
+    /* Resets the gameMatrix to 0s */
     void reset();
+
+    /* Moves a pawn from one place to another without verifications */
     void moveFromTo(int fromX, int fromY, int toX, int toY);
+
+    /* Returns true if a cell is empty */
     bool isFreeAt(int x, int y);
+
+    /* If someone has won, returns its number, 0 otherwise */
+    int hasSomeoneWon();
 };
 
 #endif
