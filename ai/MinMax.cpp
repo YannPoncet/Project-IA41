9c5c075b2ae2147149b0c30fb01704be5802a8e7
YannPoncet <yann.poncetpro@gmail.com>
2018-05-31 17:50:08 +0200

    Restart the minMax algorithm with depth and alpha beta pruning

--- ai/MinMax.cpp
+++ ai/MinMax.cpp
@@ -1,87 +1,93 @@
 #include "MinMax.hpp"
 
-vector<int> MinMax::phase2(Plateau* plateau, int player, int turn){
-  vector<int> finalDecision;
+vector<int> MinMax::minMax(Plateau* plateau, int profondeur, int player, int turn){
+  vector<int> move;
+  move.push_back(-1); //x
+  move.push_back(-1); //y
 
-  // an array to collect all the possible move
-  vector<vector<int>> moves;
-  miniMax(moves, plateau, player, turn,1);
-  cout<<"end of miniMax"<<endl;
-  
-// if it is the computer's turn loop over the moves and choose the move with the highest score
-  if(player == turn){
-    int bestScore = -10000;
-    for(unsigned int i = 0; i < moves.size(); i++){
-      if(moves[i][0] > bestScore){ //[0] = score
-        bestScore = moves[i][0];
-        finalDecision[0] = moves[i][1]; //x
-        finalDecision[1] = moves[i][2]; //y
-      }
-    }
-  }else{
+  vector<int> alphaBeta;
+  alphaBeta.push_back(std::numeric_limits<int>::min()); //alpha
+  alphaBeta.push_back(std::numeric_limits<int>::max()); //beta
 
-// else loop over the moves and choose the move with the lowest score
-    int bestScore = 10000;
-    for(unsigned int i = 0; i < moves.size(); i++){
-      if(moves[i][0] < bestScore){
-        bestScore = moves[i][0];
-        finalDecision[0] = moves[i][1]; //x
-        finalDecision[1] = moves[i][2]; //y
-      }
-    }
-  }
+  tourMax(plateau, profondeur, alphaBeta, move, player, turn);
 
-  return finalDecision;
+  return move;
 }
 
-int MinMax::miniMax(vector<vector<int>> moves, Plateau* plateau, int player, int turn, int iteration){
+int MinMax::tourMax(Plateau* plateau, int p, vector<int> alphaBeta, vector<int> move, int player, int turn){
+  if(plateau->hasSomeoneWon() || p==0)
+    return eval(plateau->getGameMatrix(), player);
 
-  cout<<iteration<<endl;
-  iteration++;
+  int u = std::numeric_limits<int>::min();
+  vector<int> action;
+  action.push_back(-1);
+  action.push_back(-1);
 
-  // loop through available spots
-  for (unsigned int i = 0; i < plateau->getGameMatrix().size(); i++){
-    for (unsigned int j = 0; j < plateau->getGameMatrix()[i].size(); j++){
-      if(plateau->getGameMatrix()[i][j]==0){ //available spot
-
-        //create an object for each and store the index of that spot
-        vector<int> move;
-      	move.push_back(0);
-        move.push_back(i);
-        move.push_back(j);
-
-        // set the empty spot to the current player
+  for(int i=0; i<5; i++){
+    for(int j=0; j<5 ;j++){
+      if(plateau->getGameMatrix()[i][j]==0){ //case vide
         plateau->getGameMatrix()[i][j] = turn;
-
-        /*collect the score resulted from calling minimax
-          on the opponent of the current player*/
-        if (player == turn){
-          int result = miniMax(moves, plateau, player, (turn%2)+1, iteration);
-          move[0] = result;
+        if(int tmp = tourMin(plateau, p-1, alphaBeta,  move, player, turn)>u){ //max
+          action[0] = i;
+          action[1] = j;
+          u = tmp;
         }
-        else{
-          int result = miniMax(moves, plateau, player, (turn%2)+1, iteration);
-          move[0] = result;
-        }
-
-        // reset the spot to empty
         plateau->getGameMatrix()[i][j] = 0;
 
-        // push the object to the array
-        moves.push_back(move);
+        if(u>alphaBeta[1]){ //cut branches
+          move[0] = action[0];
+          move[1] = action[1];
+          return u;
+        }
+
+        if(alphaBeta[0]<u)
+          alphaBeta[0]=u;
       }
     }
   }
 
-  // checks for the terminal states such as win, lose, and tie
-  //and returning a value accordingly
-  return winning(plateau, player)*10;
+  move[0] = action[0];
+  move[1] = action[1];
+  return u;
 }
 
-int MinMax::winning(Plateau* plateau, int player){
-  if(plateau->hasSomeoneWon()==player)
-    return 1;
-  else if(plateau->hasSomeoneWon()==(player%2)+1)
-    return -1;
+int MinMax::tourMin(Plateau* plateau, int p, vector<int> alphaBeta, vector<int> move, int player, int turn){
+  if(plateau->hasSomeoneWon() || p==0)
+    return eval(plateau->getGameMatrix(), player);
+
+  int u = std::numeric_limits<int>::max();
+  vector<int> action;
+  action.push_back(-1);
+  action.push_back(-1);
+
+  for(int i=0; i<5; i++){
+    for(int j=0; j<5 ;j++){
+      if(plateau->getGameMatrix()[i][j]==0){ //case vide
+        plateau->getGameMatrix()[i][j] = turn;
+        if(int tmp = tourMax(plateau, p-1, alphaBeta,  move, player, turn)<u){ //max
+          action[0] = i;
+          action[1] = j;
+          u = tmp;
+        }
+        plateau->getGameMatrix()[i][j] = 0;
+
+        if(u<alphaBeta[0]){ //cut branches
+          move[0] = action[0];
+          move[1] = action[1];
+          return u;
+        }
+
+        if(alphaBeta[1]>u)
+          alphaBeta[1]=u;
+      }
+    }
+  }
+
+  move[0] = action[0];
+  move[1] = action[1];
+  return u;
+}
+
+int MinMax::eval(vector<vector<int>> matrix, int player){
   return 0;
 }
--- ai/MinMax.hpp
+++ ai/MinMax.hpp
@@ -4,6 +4,7 @@
 #include<iostream>
 #include<vector>
 #include<math.h>
+#include <limits>
 
 #include "../model/Plateau.hpp"
 
@@ -15,10 +16,16 @@ class MinMax {
     //launches a MinMax algorithm while we are in phase 2
     static vector<int> phase2(Plateau* plateau, int player, int turn);
 
-    static int miniMax(vector<vector<int>> moves, Plateau* plateau, int player, int turn, int iteration);
+    //launches the minMax algorithm and returns the move to do
+    vector<int> minMax(Plateau* plateau, int profondeur, int player, int turn);
 
-    //tells if a player wins or if noone wins
-    static int winning(Plateau* plateau, int turn);
+    //the max part of the algorithm
+    int tourMax(Plateau* plateau, int p, vector<int> alphaBeta, vector<int> move, int player, int turn);
+    //the min part of the algorithm
+    int tourMin(Plateau* plateau, int p, vector<int> alphaBeta, vector<int> move, int player, int turn);
+    //used to tell how much points we give to a plateau :D
+    int eval(vector<vector<int>> matrix, int player);
+
 };
 
 #endif
