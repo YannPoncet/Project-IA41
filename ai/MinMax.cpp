61c69133302a534a9512b5292e10110eb76fa1fc
BorisBrogle <boris.brogle@gmail.com>
2018-05-31 18:03:20 +0200

    Eval

--- ai/MinMax.cpp
+++ ai/MinMax.cpp
@@ -7,7 +7,7 @@ vector<int> MinMax::phase2(Plateau* plateau, int player, int turn){
   vector<vector<int>> moves;
   miniMax(moves, plateau, player, turn,1);
   cout<<"end of miniMax"<<endl;
-  
+
 // if it is the computer's turn loop over the moves and choose the move with the highest score
   if(player == turn){
     int bestScore = -10000;
@@ -85,3 +85,81 @@ int MinMax::winning(Plateau* plateau, int player){
     return -1;
   return 0;
 }
+
+
+
+int MinMax::eval(vector<vector<int>> gameMatrix, int player) {
+  bool firstTested = true;
+  bool secondTested = true;
+  int score1 = 0;
+  int score2 = 0;
+  int score = 0;
+
+  for(int x=0; x<5; x++) {
+    for(int y=0; y<5; y++) {
+      int currentVal = gameMatrix[x][y];
+      vector<int> result;
+      if(firstTested && currentVal == 1) {
+        result = findPattern(gameMatrix, x, y, 1);
+      }
+      if(secondTested && currentVal == 2) {
+        result = findPattern(gameMatrix, x, y, 2);
+      }
+    }
+  }
+
+  if(player == 1) {
+    score = score1 - score2;
+  } else {
+    score = score2 - score1;
+  }
+
+  return score;
+}
+
+vector<int> MinMax::findPattern(vector<vector<int>> gameMatrix, int x, int y, int player) {
+  std::unordered_map<int, int[3][2]> pattern4 = {
+    {100, {{1,1},{2,2},{3,3}} }, //diag
+    {100, {{1,0},{0,1},{1,1}} }, //square
+    {100, {{1,0},{2,0},{3,0}} }}; //line
+  std::unordered_map<int, int[2][2]> pattern3 = {
+    {10, {{1,0},{1,1}} }, //square
+    {10, {{1,0},{2,0}} }, //line
+    {10, {{1,1},{2,2}} }}; //diag
+  std::unordered_map<int, int[1][2]> pattern2 = {
+    {1, {{1,1}} }, //diag
+    {1, {{1,0}} }}; //line
+
+
+  vector<int> result;
+
+  /*for(int pat=0; pat<(signed)pattern4.size(); pat++) {
+    bool testBool = true;
+    int coord = 0;
+    do {
+      int testX = x+pattern4[pat][coord][0];
+      int testY = y+pattern4[pat][coord][1];
+      if(testX>=0 && testX<this->nbCells && testY>=0 && testY<this->nbCells) { //if we're outside of the gameMatrix
+        if(this->gameMatrix[testX][testY] != currentVal) {
+          testBool = false;
+        }
+      } else {
+        testBool = false;
+      }
+      coord++;
+    } while(testBool == true && coord <3);
+
+    if(testBool == true) { //if test bool is still at true, we have found a victory configuration
+      return currentVal;
+    }
+  }
+
+*/
+  return {0,0};
+}
+
+/*bool testAPattern(vector<vector<int>> gameMatrix, int x, int y, int player)
+{
+
+}
+*/
--- ai/MinMax.hpp
+++ ai/MinMax.hpp
@@ -4,6 +4,7 @@
 #include<iostream>
 #include<vector>
 #include<math.h>
+#include<unordered_map>
 
 #include "../model/Plateau.hpp"
 
@@ -19,6 +20,11 @@ class MinMax {
 
     //tells if a player wins or if noone wins
     static int winning(Plateau* plateau, int turn);
+
+    //returns the evalutation of a state of the game
+    static int eval(vector<vector<int>> gameMatrix, int player);
+    //used in eval to locate a pattern
+    static vector<int> findPattern(vector<vector<int>> gameMatrix, int x, int y, int player);
 };
 
 #endif
